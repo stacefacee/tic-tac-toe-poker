01a51fdf8f4921ddb87e515456f2d4e4b27c718d
stacefacee <81988886+stacefacee@users.noreply.github.com>
2021-04-15 09:36:05 -0700

    commit

---
+++ Part2/index.js
@@ -0,0 +1,15 @@
+const idInput = document.getElementById('idInput');
+const colorInput = document.getElementById('colorInput');
+
+function setCard() {
+  const card = document.getElementById(idInput.value);
+  card.style.color = colorInput.value;
+}
+
+const idInput = document.getElementById('idInput');
+const colorInput = document.getElementById('colorInput');
+
+function setCard() {
+  const card = document.getElementById(idInput.value);
+  card.style.color = colorInput.value;
+}
